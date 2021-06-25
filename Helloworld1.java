4b08a052bb60d61f3de79797247cf6012c1a51cb
MdMuqeem <mdmuqeemcbr396@gmail.com>
2021-06-25 23:06:27 +0530

    A role is added

--- Helloworld1.java
+++ Helloworld1.java
@@ -1,8 +1,9 @@
 /**
-* @wi.implements CSV4template/CSV-1157:2983 Returns author of this book
+* @wi.parent CSV4template/CSV-1157:2983 Returns author of this book
 */
 class Helloworld1 {  
     public static void main(String args[]){  
-     System.out.println("This is to test in polarion from jenkins");  
+     System.out.println("This is to test in polarion from jenkins");
+     System.out.println("Parent role have been added");  
     }  
 }  
